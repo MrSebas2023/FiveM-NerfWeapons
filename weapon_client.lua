eb082e557b1dd9bb98d7bad020ea3d420f6d507e
Sebastiaan de Bruin <se-basdebruin2@hotmail.com>
2020-07-11 11:54:07 +0200

    Update weapon_client.lua

--- weapon_client.lua
+++ weapon_client.lua
@@ -1,9 +1,12 @@
 Citizen.CreateThread(function()
     while true do
-				N_0x4757f00bc6323cfe(GetHashKey("WEAPON_COMBATPISTOL"), 0.4)
-				Wait(0)
-				-- To add more weapons use template below
-				--N_0x4757f00bc6323cfe(GetHashKey([Weapon spawn name]), 0.4)
-				--Wait(0)
+			
+	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_COMBATPISTOL"), 0.4)
+	Wait(0)
+			
+	-- To add more weapons use template below
+	--N_0x4757f00bc6323cfe(GetHashKey([Weapon spawn name]), 0.4)
+	--Wait(0)
+			
     end
 end)
